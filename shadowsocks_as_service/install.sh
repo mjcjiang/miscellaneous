38c71ffd4e99d71579ae9b32b02894e9d2ecab27
mjcjiang <redmagic039@gmail.com>
2023-08-18 05:26:09 +0000

    code commit

--- shadowsocks_as_service/install.sh
+++ shadowsocks_as_service/install.sh
@@ -1,1 +1,5 @@
 sudo apt install shadowsocks-libev
+
+# set git use socks5 as proxy
+git config --global http.proxy 'socks5://127.0.0.1:1080'
+git config --global https.proxy 'socks5://127.0.0.1:1080'
