2c7ddb7f665b9e96cc49e3db8e187034fd924ae9
mjcjiang <redmagic039@gmail.com>
2023-08-24 17:09:33 +0800

    small change

--- emacs/init.el
+++ emacs/init.el
@@ -264,4 +264,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
     '(default ((t (:family "Monaco" :foundry "APPL" :slant normal :weight normal :height 143 :width normal)))))
- 
